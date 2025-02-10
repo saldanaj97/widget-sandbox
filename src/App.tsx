b685f9d583603cba9c859b13310a88b6fd40ad94
saldanaj97 <saldanaj97@gmail.com>
2025-02-10 14:42:08 -0600

    add gap between elements in Clock Widget layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Clock } from "./Clock";
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-8">
         <h1 className="text-6xl font-bold">Clock Widget</h1>
         <Clock />
       </div>
