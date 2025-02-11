91a84193c3481f00f5f09e47592938c447f5e24b
saldanaj97 <saldanaj97@gmail.com>
2025-02-11 11:35:01 -0600

    added button to switch between the widget components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 import { Clock } from "./Clock";
 
 function App() {
+  const [widgetToDisplay, setWidgetToDisplay] = useState("clock");
+
+  const WidgetButtons = () => {
+    return (
+      <>
+        {widgetToDisplay !== "clock" && (
+          <button
+            className="rounded-2xl bg-black p-4"
+            onClick={() => setWidgetToDisplay("clock")}
+          >
+            Clock
+          </button>
+        )}
+        {widgetToDisplay !== "calender" && (
+          <button
+            className="rounded-2xl bg-black p-4"
+            onClick={() => setWidgetToDisplay("calender")}
+          >
+            Calender
+          </button>
+        )}
+      </>
+    );
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center gap-8">
-        <h1 className="text-6xl font-bold">Clock Widget</h1>
-        <Clock />
+        {widgetToDisplay === "clock" && (
+          <>
+            <h1 className="text-6xl font-bold">Clock Widget</h1>
+            <Clock />
+          </>
+        )}
+        <WidgetButtons />
       </div>
     </>
   );
